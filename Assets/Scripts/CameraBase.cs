1efbe4cc3e91993c232b9008198fad81494b1ad9
andewth <dewshelldon@gmail.com>
2026-01-21 21:00:49 +0700

    Week 1

--- Assets/Scripts/CameraBase.cs
+++ Assets/Scripts/CameraBase.cs
@@ -2,41 +2,33 @@ using UnityEngine;
 
 public class CameraBase : MonoBehaviour
 {
-    [SerializeField] private CameraController cam;
-
     [Header("Move")]
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float moveSpeed = 50f;
 
-    [SerializeField] private float xInput;
-    [SerializeField] private float zInput;
+    private float xInput;
+    private float zInput;
 
     public static CameraBase instance;
 
-    void Start()
-    {
-        moveSpeed = 50;
-    }
-
-
     void Awake()
     {
-        instance = this;
-        cam = Camera.main;
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
     }
-
 
     void Update()
     {
         MoveByKB();
     }
 
-
     private void MoveByKB()
     {
         xInput = Input.GetAxis("Horizontal");
         zInput = Input.GetAxis("Vertical");
 
-        Vecter3 dir = (transform.forward * zInput) + (transform.right * xInput);
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
         transform.position += dir * moveSpeed * Time.deltaTime;
     }
 }
