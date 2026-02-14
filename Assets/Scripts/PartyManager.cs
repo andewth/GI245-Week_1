34cd460f29042a33712fa4aaa2166eb167297704
andewth <dewshelldon@gmail.com>
2026-02-14 09:11:30 +0700

    Party Script

---
+++ Assets/Scripts/PartyManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyManager : MonoBehaviour
+{
+    [SerializeField] List<Character> selectChars = new List<Character>();
+    public List<Character> selectChars { get { return selectChars } }
+    
+
+    public static PartyManager instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
