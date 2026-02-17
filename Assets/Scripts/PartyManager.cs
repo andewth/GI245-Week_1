471263722cb2061ebd8039364e3e10d2781d68df
andewth <dewshelldon@gmail.com>
2026-02-18 00:42:24 +0700

    update

--- Assets/Scripts/PartyManager.cs
+++ Assets/Scripts/PartyManager.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 using System.Collections.Generic; // List [Need]
 
@@ -18,28 +17,4 @@ public class PartyManager : MonoBehaviour
     {
         instance = this;
     }
-=======
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PartyManager : MonoBehaviour
-{
-    [SerializeField] List<Character> selectChars = new List<Character>();
-    public List<Character> SelectChars { get { return selectChars; } }
-    
-
-    public static PartyManager instance;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
-    {
-        instance = this;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
->>>>>>> c17e185ca2ac52f5962968a9ab73b1617dcc89b6
 }
